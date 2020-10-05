3d0f5b6fb303615bfde0182236ed9b2ff1982595
alain <alrouen@live.fr>
2020-10-05 22:57:36 +0200

    very beginning of the front-end : fermentation chart experimentation

--- brew-dashboard/src/components/fermentation.js
+++ brew-dashboard/src/components/fermentation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from "urql";
-import ReactDOM from 'react-dom';
-import { VictoryChart, VictoryLine, VictoryTheme, VictoryAxis } from 'victory';
+import moment from "moment";
+import { VictoryChart, VictoryLine, VictoryTheme, VictoryAxis, VictoryLabel, VictoryTooltip, VictoryVoronoiContainer } from 'victory';
 
 const fermentationData = `
     query {
@@ -10,6 +10,11 @@ const fermentationData = `
 `;
 
 // Array of :  { "_ts": 1601811104, "t": 23.656, "p": 0, "v": 2.68, "__typename": "fermentingData" },
+
+const toStrDate = (t) => {
+    const date = moment.unix(t);
+    return date.format('DD/MM/YYYY HH:mm')
+}
 
 export default () => {
 
@@ -26,21 +31,22 @@ export default () => {
         } else {
 
             const samples = res.data.fermentingTSBySessionId.record;
+            const maxTemp = 40;//Math.max(...samples.map(d => d.t))*1.5;
+            const maxPressure = 600;//Math.max(...samples.map(d => d.p))*1.5;
 
             return <div>
-                <VictoryChart theme={VictoryTheme.material} >
-                    <VictoryLine data={samples} x="_ts" y="p" style={{ data: { stroke: "red" } }} />
-                    <VictoryLine data={samples} x="_ts" y="t" style={{ data: { stroke: "blue" } }}/>
+                <VictoryChart theme={VictoryTheme.material} width={800} height={400} containerComponent={
+                    <VictoryVoronoiContainer
+                        labels={({ datum }) => `${(datum.t).toFixed(2)}°C, ${(datum.p).toFixed(2)} mBar\n ${toStrDate(datum._ts)}`}
+                    />
+                }>
+                    <VictoryAxis fixLabelOverlap={true} tickFormat={(t) => toStrDate(t)} label="Date" axisLabelComponent={<VictoryLabel dy={30}/>} />
+                    <VictoryAxis dependentAxis label="Temperature (°C)" axisLabelComponent={<VictoryLabel dy={-30}/>} tickValues={[0.25, 0.5, 0.75, 1]} tickFormat={(t) => (t * maxTemp).toFixed(0)} />
+                    <VictoryLine data={samples} x="_ts" y={(d) => (d.t / maxTemp)}  style={{ data: { stroke: "blue" } }}/>
+                    <VictoryAxis orientation="right" dependentAxis label="Pressure (mBar)" axisLabelComponent={<VictoryLabel dy={30}/>} tickValues={[0.25, 0.5, 0.75, 1]} tickFormat={(t) => (t * maxPressure).toFixed(0)} />
+                    <VictoryLine data={samples} x="_ts" y={(d) => d.p / maxPressure} style={{ data: { stroke: "red" } }} />
                 </VictoryChart>
             </div>
         }
     }
 }
-
-/*
-{JSON.stringify(
-                    res.data.fermentingTSBySessionId.record,
-                    null,
-                    2
-                ) }
- */
